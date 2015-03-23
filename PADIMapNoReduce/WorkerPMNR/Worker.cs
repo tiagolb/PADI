36b30a1a5562c791dcd537ad5b291dc167f05c94
Tiago Brito <tiagoluisbrito@gmail.com>
2015-03-23 15:59:12 +0000

    [ADD] Added SendMapper method to worker

--- PADIMapNoReduce/WorkerPMNR/Worker.cs
+++ PADIMapNoReduce/WorkerPMNR/Worker.cs
@@ -7,6 +7,7 @@ using System.Runtime.Remoting;
 using System.Runtime.Remoting.Channels;
 using System.Runtime.Remoting.Channels.Tcp;
 using System.Runtime.Remoting.Channels.ChannelServices;
+using System.Reflection;
 
 
 namespace WorkerPMNR {
@@ -26,14 +27,38 @@ namespace WorkerPMNR {
 
             int port = Int32.Parse(Console.ReadLine());
             Worker worker = new Worker(port);
-
-
         }
 
     }
 
     public class RemoteWorker : MarshalByRefObject {
-       
+        public bool SendMapper(byte[] code, string className) {
+            Assembly assembly = Assembly.Load(code);
+            // Walk through each type in the assembly looking for our class
+            foreach (Type type in assembly.GetTypes()) {
+                if (type.IsClass == true) {
+                    if (type.FullName.EndsWith("." + className)) {
+                        // create an instance of the object
+                        object ClassObj = Activator.CreateInstance(type);
+
+                        // Dynamically Invoke the method
+                        object[] args = new object[] { "testValue" };
+                        object resultObject = type.InvokeMember("Map",
+                          BindingFlags.Default | BindingFlags.InvokeMethod,
+                               null,
+                               ClassObj,
+                               args);
+                        IList<KeyValuePair<string, string>> result = (IList<KeyValuePair<string, string>>)resultObject;
+                        Console.WriteLine("Map call result was: ");
+                        foreach (KeyValuePair<string, string> p in result) {
+                            Console.WriteLine("key: " + p.Key + ", value: " + p.Value);
+                        }
+                        return true;
+                    }
+                }
+            }
+            throw (new System.Exception("could not invoke method"));
+        }
     }
 
 }

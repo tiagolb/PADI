4cf3747d8a97c3afa7251288d44285f971fc714c
Diogo Barradas <diogo.barradas@outlook.com>
2015-03-23 10:34:22 +0000

    Lol revisited

--- PADIMapNoReduce/PuppetMaster/PuppetMasterForm.cs
+++ PADIMapNoReduce/PuppetMaster/PuppetMasterForm.cs
@@ -15,7 +15,7 @@ namespace PuppetMaster {
         }
 
         private void bt_script_Click(object sender, EventArgs e) {
-            ofd_script.Filter = "TXT|*.txt" ;
+            ofd_script.Filter = "TXT|*.txt";
             if (ofd_script.ShowDialog() == DialogResult.OK) {
                 tb_scriptFileName.Text = ofd_script.SafeFileName;
                 tb_scriptFileAddress.Text = ofd_script.FileName;
